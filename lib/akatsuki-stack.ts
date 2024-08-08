950051559ac0a6eb2e91b8cba3b059279d31d6f4
Kei Sawamura <kei@9th.tokyo>
2024-08-09 01:25:17 +0900

    Initialize develop environment

--- lib/akatsuki-stack.ts
+++ lib/akatsuki-stack.ts
@@ -1,16 +1,22 @@
-import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { Stack, StackProps, CfnOutput } from 'aws-cdk-lib';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export class AkatsukiStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class AkatsukiStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    const helloFunction = new lambda.Function(
+        this, 'HelloFunction', {
+            runtime: lambda.Runtime.NODEJS_20_X,
+            handler: 'hello.handler',
+            functionName: 'HelloFunction',
+            code: lambda.Code.fromAsset('lambdas/hello'),
+        });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AkatsukiQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
   }
 }
