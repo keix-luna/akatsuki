8588358e45a73047db183a0a3d37b79223c6fe5f
Kei Sawamura <kei@9th.tokyo>
2024-08-10 03:24:14 +0900

    Implement lambda function

---
+++ lambdas/setsugetsuka/app.ts
@@ -0,0 +1,62 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import { SSM } from 'aws-sdk';
+
+async function getApiKey(parameterName: string): Promise<string> {
+  const ssm = new SSM();
+  const apiKeyParam = await ssm.getParameter({
+    Name: parameterName,
+  }).promise();
+
+  const apiKey = apiKeyParam.Parameter?.Value;
+  if (!apiKey) {
+    throw new Error('API key is undefined');
+  }
+
+  return apiKey;
+}
+
+async function createGenAIInstance(): Promise<GoogleGenerativeAI> {
+  const apiKey = await getApiKey('/keix/api-key/gemini');
+  return new GoogleGenerativeAI(apiKey);
+}
+
+export const handler =
+  async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+
+  try {
+
+    const genAI = await createGenAIInstance();
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+    const prompt = "The response about the foods in Thailand and the words is limit 200 in Japanese.";
+    const result = await model.generateContent(prompt);
+    const response = await result.response;
+    const text = await response.text();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        project: 'Akatsuki',
+        engine: 'Gemini',
+        function: 'SetsuGetsuKa',
+        text: text,
+        version: 'v1',
+      }), 
+    };
+  } catch (error) {
+
+    let errorMessage = 'An error occurred';
+    if (error instanceof Error) {
+      errorMessage = error.message;
+    }
+
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: errorMessage,
+        error: error instanceof Error ? error.stack : 'Unknown error'
+      }),
+    };
+  }
+};
